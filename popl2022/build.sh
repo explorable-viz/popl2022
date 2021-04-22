3e48adf87cbbdfe1386289e65d02f18e92070d91
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-22 14:33:25 +0100

    Pull down source files into fluid dir.

--- popl2022/build.sh
+++ popl2022/build.sh
@@ -8,6 +8,14 @@ cd $HERE
 PDFLATEX="pdflatex -file-line-error -halt-on-error"
 TARGET=main
 
+rm -rf fluid
+mkdir fluid
+pushd fluid
+wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
+popd
+
+exit 0
+
 $PDFLATEX $TARGET
 #	bibtex $TARGET
 #	$PDFLATEX $TARGET
