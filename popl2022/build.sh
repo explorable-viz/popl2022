59f42dc8f26404f19754526e3af73dd624f3230d
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-22 17:40:17 +0100

    Extract post_process function.

--- popl2022/build.sh
+++ popl2022/build.sh
@@ -8,16 +8,21 @@ cd $HERE
 PDFLATEX="pdflatex -file-line-error -halt-on-error"
 TARGET=main
 
+post_process () {
+   local bak='.bak'
+   sed -i $bak 's/>=/$\\numgeq$/' $1
+   sed -i $bak 's/<=/$\\numleq$/' $1
+   sed -i $bak 's/<-/$\\leftarrow$/' $1
+   sed -i $bak 's/\[|/\$\\langle$/' $1
+   sed -i $bak 's/\|]/$\\rangle$/' $1
+   rm $1$bak
+}
+
 rm -rf fluid
 mkdir fluid
 pushd fluid
 wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
-sed -i .bak 's/>=/$\\numgeq$/' convolution.fld
-sed -i .bak 's/<=/$\\numleq$/' convolution.fld
-sed -i .bak 's/<-/$\\leftarrow$/' convolution.fld
-sed -i .bak 's/\[|/\$\\langle$/' convolution.fld
-sed -i .bak 's/\|]/$\\rangle$/' convolution.fld
-rm *.bak
+post_process convolution.fld
 popd
 
 $PDFLATEX $TARGET
