4d4dd2c8fd2148dc1b51758ee3697f21e932f7c6
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-22 17:49:14 +0100

    Extract example processing into separate script.

--- popl2022/build.sh
+++ popl2022/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e # not perfect but better than nothing
+set -e
 
 # make sure script runs in directory that contains it
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -8,24 +8,7 @@ cd $HERE
 PDFLATEX="pdflatex -file-line-error -halt-on-error"
 TARGET=main
 
-post_process () {
-   local bak='.bak'
-   sed -i $bak 's/>=/$\\numgeq$/' $1
-   sed -i $bak 's/<=/$\\numleq$/' $1
-   sed -i $bak 's/<-/$\\leftarrow$/' $1
-   sed -i $bak 's/\[|/\$\\langle$/' $1
-   sed -i $bak 's/\|]/$\\rangle$/' $1
-   rm $1$bak
-}
-
-rm -rf fluid
-mkdir fluid
-pushd fluid
-wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
-post_process convolution.fld
-wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/example/slicing/conv-extend.fld
-post_process conv-extend.fld
-popd
+. collate-examples.sh
 
 $PDFLATEX $TARGET
 #	bibtex $TARGET
---
+++ popl2022/collate-examples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+post_process () {
+   local bak='.bak'
+   sed -i $bak 's/>=/$\\numgeq$/' $1
+   sed -i $bak 's/<=/$\\numleq$/' $1
+   sed -i $bak 's/<-/$\\leftarrow$/' $1
+   sed -i $bak 's/\[|/\$\\langle$/' $1
+   sed -i $bak 's/\|]/$\\rangle$/' $1
+   rm $1$bak
+}
+
+rm -rf fluid
+mkdir fluid
+pushd fluid
+wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
+post_process convolution.fld
+wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/example/slicing/conv-extend.fld
+post_process conv-extend.fld
+popd
