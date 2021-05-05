48c7973a5faa1534ccc71d858ecf38d43770f7af
Roly Perera <roly.perera@dynamicaspects.org>
2021-05-05 11:42:17 +0100

    Go back to Ubuntu for now and park this issue (although Windows was noticably faster).

--- popl2022/collate-examples.sh
+++ popl2022/collate-examples.sh
@@ -8,16 +8,17 @@ post_process () {
 rm -rf fluid
 mkdir fluid
 pushd fluid
-# wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
-# post_process convolution.fld
-# wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/example/slicing/conv-extend.fld
-# post_process conv-extend.fld
+wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/lib/convolution.fld
+post_process convolution.fld
+wget https://raw.githubusercontent.com/explorable-viz/fluid/master/fluid/example/slicing/conv-extend.fld
+post_process conv-extend.fld
 popd
 
+# Sounds like ChromeDriver needs to be configured to enable headless downloads, but not sure how to do that.
 download_image () {
    ext=png
    folder=~/Downloads
-   image=$folder/$1.$2
+   image=$folder/$1.$ext
    echo $image
    chrome --headless --user-data-dir=C:/tmp --disable-gpu --no-sandbox http://f.luid.org/new/
    until [ -f $image ]
@@ -29,4 +30,4 @@ download_image () {
    exit
 }
 
-download_image image
+# download_image image
