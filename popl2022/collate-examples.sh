cb09842c237aed79150f1e9e2ed1844ccabe97c0
Roly Perera <roly.perera@dynamicaspects.org>
2021-05-05 10:05:46 +0100

    Try 'chrome' again, now we're running outside of Docker.

--- popl2022/collate-examples.sh
+++ popl2022/collate-examples.sh
@@ -18,7 +18,7 @@ download_image () {
    ext=png
    image=~/Downloads/$1.$2
    rm -f $image
-   chrome-browser http://f.luid.org/new/
+   chrome http://f.luid.org/new/
    until [ -f $image ]
    do
         sleep 1
