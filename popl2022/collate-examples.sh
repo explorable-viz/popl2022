60ee353abc1cd1c3e624f129b429078a883fffca
Roly Perera <roly.perera@dynamicaspects.org>
2021-05-05 10:11:43 +0100

    Try chrome --headless.

--- popl2022/collate-examples.sh
+++ popl2022/collate-examples.sh
@@ -18,7 +18,7 @@ download_image () {
    ext=png
    image=~/Downloads/$1.$2
    rm -f $image
-   chrome http://f.luid.org/new/
+   chrome --headless http://f.luid.org/new/
    until [ -f $image ]
    do
         sleep 1
