ee1377f6acb7817bcb78f64ff0a323f505bfd608
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-19 12:05:46 +0100

    Move 'cd HERE' malarky to all-versions.

--- popl2022/build.sh
+++ popl2022/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# make sure script runs in directory that contains it
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $HERE
-
 PDFLATEX="pdflatex -file-line-error -halt-on-error"
 TARGET=${1:-anonymised}
 echo Building target \"$TARGET\".
