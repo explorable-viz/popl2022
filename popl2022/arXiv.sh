6b9666649836fb17895844050e7d490c9b890e9b
Roly Perera <roly.perera@dynamicaspects.org>
2021-09-01 15:33:03 +0100

    Building in .bbl.

--- popl2022/arXiv.sh
+++ popl2022/arXiv.sh
@@ -2,6 +2,13 @@
 set -e
 
 NAME=arXiv.zip
+TARGET=final-full
+
+pdflatex $TARGET
+bibtex $TARGET
+
+rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc
+
 pushd ..
 
 zip -r $NAME popl2022 tex-common
@@ -9,8 +16,11 @@ zip -r $NAME popl2022 tex-common
 zip -d $NAME *.pdf
 zip -d $NAME *.DS_Store
 zip -d $NAME *.git
+zip -d $NAME *.gitignore
 
 ls -al $NAME
 unzip -l $NAME
 
 popd
+
+rm -f $TARGET.bbl
