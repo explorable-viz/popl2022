396fc86e42a1722cb868a53ffcdee910d903b326
Roly Perera <roly.perera@dynamicaspects.org>
2021-09-01 15:22:05 +0100

    Massive nause.

--- popl2022/arXiv.sh
+++ popl2022/arXiv.sh
@@ -2,9 +2,15 @@
 set -e
 
 NAME=arXiv.zip
-zip -r $NAME .
-zip -d $NAME *.pdf
-zip -d $NAME fig/.DS_Store
-zip -d $NAME .DS_Store
+pushd ..
 
+zip -r $NAME popl2022 tex-common
+
+zip -d $NAME *.pdf
+zip -d $NAME *.DS_Store
+zip -d $NAME *.git
+
+ls -al $NAME
 unzip -l $NAME
+
+popd
